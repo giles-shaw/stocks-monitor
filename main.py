b4a7b63b4ba3facccbf73b5c4d6017571a6db194
Giles Shaw <giles.shaw@gmail.com>
2019-04-22 10:53:54 +0100

    First working version with threading

--- main.py
+++ main.py
@@ -1,45 +1,78 @@
 import asyncio
+import random
 import threading
 from datetime import datetime
 from sys import argv
 from time import sleep
+from typing import Any, Dict, List, Union
 
 import requests
 import urwid
 
 iex_batch_url = "https://api.iextrading.com/1.0/stock/market/batch"
 
-symbols = argv
-symbols = ",".join(symbols)
-payload = {"symbols": symbols, "types": "price"}
-
-msg = urwid.Text("Fetching data...")
-
 
 def exit_on_any_key(_):
     raise urwid.ExitMainLoop()
 
 
-def refresh():
+# def update_stock_info(info) -> Dict[str, Union[str, float]]:
+#
+#     while True:
+#         payload = requests.get(
+#             iex_batch_url, params={
+#                 "symbols": ",".join(symbols), "types": "price"}
+#         ).json()
+#         for symbol in info.keys():
+#             info[symbol] = payload[symbol]
+#         sleep(10)
+
+
+def update_stock_info(info) -> Dict[str, Union[str, float]]:
 
     while True:
-        r = requests.get(iex_batch_url, params=payload)
-        r = r.json()
-
-        txt = ""
-        for ticker, details in r.items():
-            txt = txt + f'{ticker}: {details["price"]}\n'
-
-        txt += f'Time is: {datetime.now().strftime("%X")}\n'
-        txt += "Press any key to exit."
-        msg.set_text(txt)
+        for symbol in info.keys():
+            info[symbol] = {"price": random.randint(1, 101)}
         sleep(1)
 
 
-watcher = threading.Thread(target=refresh)
-# refresh(0)
-fill = urwid.Filler(msg, "top")
-loop = urwid.MainLoop(fill, unhandled_input=exit_on_any_key)
+def refresh(loop: urwid.MainLoop, args):
 
-watcher.start()
-loop.run()
+    info, fill, msg = args
+    txt = ""
+    for ticker, details in info.items():
+        txt = txt + f'{ticker}: {details["price"]}\n'
+    txt += "Press any key to exit."
+    msg.set_text(txt)
+    loop.draw_screen()
+    loop.set_alarm_in(1, refresh, (info, fill, msg))
+
+
+def gui(info: Dict[str, Union[str, float]]):
+
+    msg = urwid.Text("Fetching data...")
+    fill = urwid.Filler(msg, "top")
+    loop = urwid.MainLoop(fill, unhandled_input=exit_on_any_key)
+    loop.set_alarm_in(0, refresh, (info, fill, msg))
+    loop.run()
+
+
+def stocks_monitor(symbols: List[str]):
+
+    info = dict((s, {"price": None}) for s in symbols)
+    updater = threading.Thread(target=update_stock_info, args=(info,))
+    updater.daemon = True
+    updater.start()
+
+    gui(info)
+
+
+def cli():
+
+    symbols = argv[1:]
+    print(symbols)
+    stocks_monitor(symbols)
+
+
+if __name__ == "__main__":
+    cli()

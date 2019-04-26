37b69ad8a24e9cef79271299ed15c9f58c04aeb5
Giles Shaw <giles.shaw@gmail.com>
2019-04-26 22:38:07 +0100

    Encapsulate prices in an object

--- main.py
+++ main.py
@@ -16,18 +16,6 @@ def exit_on_any_key(_):
     raise urwid.ExitMainLoop()
 
 
-def update_stock_info(info) -> Dict[str, Union[str, float]]:
-
-    while True:
-        payload = requests.get(
-            iex_batch_url,
-            params={"symbols": ",".join(list(info.keys())), "types": "price"},
-        ).json()
-        for symbol in info.keys():
-            info[symbol] = payload[symbol]
-        sleep(1)
-
-
 # def update_stock_info(info) -> Dict[str, Union[str, float]]:
 #
 #     while True:
@@ -36,41 +24,68 @@ def update_stock_info(info) -> Dict[str, Union[str, float]]:
 #         sleep(1)
 
 
+class Prices:
+    def __init__(self, symbols):
+        self.symbols = symbols
+        self.prices = self.get_prices()
+        if self.prices is None:
+            raise ValueError("prices is None")
+
+    def get_prices(self):
+        r = requests.get(
+            iex_batch_url, params={"symbols": ",".join(
+                self.symbols), "types": "price"}
+        )
+        if r.status_code // 100 == 2:
+            return r.json()
+        else:
+            raise Error()
+
+    def get_fake_prices(self):
+        for details in self.prices.values():
+            details["price"] += 1
+        return self.prices
+
+    def update_loop(self, delay=10):
+        while True:
+            self.prices = self.get_fake_prices()
+            sleep(delay)
+
+
 def refresh(loop: urwid.MainLoop, args):
 
-    info, fill, msg = args
+    prices, fill, msg = args
     txt = ""
-    for ticker, details in info.items():
+    for ticker, details in prices.prices.items():
         txt = txt + f'{ticker}: {details["price"]}\n'
     txt += "Press any key to exit."
     msg.set_text(txt)
     loop.draw_screen()
-    loop.set_alarm_in(1, refresh, (info, fill, msg))
+    loop.set_alarm_in(1, refresh, (prices, fill, msg))
 
 
-def gui(info: Dict[str, Union[str, float]]):
+def gui(prices: Prices):
 
-    msg = urwid.Text("Fetching data...")
+    msg = urwid.Text("Press any key to exit.")
     fill = urwid.Filler(msg, "top")
     loop = urwid.MainLoop(fill, unhandled_input=exit_on_any_key)
-    loop.set_alarm_in(0, refresh, (info, fill, msg))
+    loop.set_alarm_in(0, refresh, (prices, fill, msg))
     loop.run()
 
 
 def stocks_monitor(symbols: List[str]):
 
-    info = dict((s, {"price": None}) for s in symbols)
-    updater = threading.Thread(target=update_stock_info, args=(info,))
+    prices = Prices(symbols)
+    updater = threading.Thread(target=prices.update_loop)
     updater.daemon = True
     updater.start()
 
-    gui(info)
+    gui(prices)
 
 
 def cli():
 
     symbols = argv[1:]
-    print(symbols)
     stocks_monitor(symbols)
 
 

66f82c5ef8a24d28ac7f56e7b0fb320e953f4714
Giles Shaw <giles.shaw@gmail.com>
2019-05-25 19:14:22 +0100

    First setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="stocks-monitor",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Giles Shaw",
+    author_email="giles.shaw@gmail.com",
+    description="A simple stock monitor for the command line.",
+    entry_points={
+        "console_scripts": ["stocks-monitor=stocks_monitor.main:cli"]
+    },
+)

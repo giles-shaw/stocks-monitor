48fc62acee4b0af00c81e3e06aba35057c2fb606
Giles Shaw <giles.shaw@gmail.com>
2019-06-12 22:16:42 +0100

    Refactor so that all widget column updates occur in a single thread

--- stocks_monitor/dataframe_widget.py
+++ stocks_monitor/dataframe_widget.py
@@ -2,62 +2,42 @@
 View a pd.DataFrame whose values are numbers or strings using an urwid widget
 with the ability to sort rows according to user input.
 """
-from threading import Lock
+from queue import Queue
 from typing import Any, Callable
 
 import pandas as pd
 import urwid
 
 from stocks_monitor.format import format_df
-from stocks_monitor.sort import sort_direction, df_sort_status, SortKey
+from stocks_monitor.sort import SortKey
 
 
 class DataFrameWidget(urwid.Filler):
     def __init__(self, data: pd.DataFrame = pd.DataFrame([])) -> None:
         original_widget = urwid.Columns([], dividechars=3)
         super().__init__(original_widget, "top")
-        self.data = data
-        self.lock = Lock()
 
-        self._column_sort_status = df_sort_status(self.data)
-        self.sort_columns(sort_key=0, user_input=False)
+    def update_columns(self, data: pd.DataFrame) -> None:
 
-    def sort_columns(self, sort_key: int, user_input: bool) -> None:
+        self.original_widget.contents = [
+            (c, self.original_widget.options("pack")) for c in format_df(data)
+        ]
 
-        with self.lock:
-            sorted_df = self.data.sort_values(
-                by=self.data.columns[sort_key],
-                ascending=self._sort_direction(sort_key, user_input),
-            )
-            self._column_sort_status = df_sort_status(sorted_df)
-
-            self.original_widget.contents = [
-                (c, self.original_widget.options("pack"))
-                for c in format_df(sorted_df)
-            ]
-
-    def handle_input(self, sort_key: SortKey) -> Callable[[Any], bool]:
+    def handle_input(self, queue: Queue) -> Callable[[Any], bool]:
         def fn(key: Any) -> bool:
             if key in ("q", "Q"):
                 raise urwid.ExitMainLoop()
             # Ensure that input is a valid sort key.
             try:
-                assert int(key) - 1 in range(self.data.shape[1])
+                assert int(key) - 1 in range(
+                    len(self.original_widget.contents)
+                )
             except (ValueError, TypeError):
                 return False
             except AssertionError:
                 return False
 
-            sort_key.sort_key = int(key) - 1
-            self.sort_columns(sort_key.sort_key, user_input=True)
+            queue.put(SortKey(int(key) - 1))
             return True
 
         return fn
-
-    def _sort_direction(self, sort_key: int, user_input: bool):
-
-        return sort_direction(
-            self.data.iloc[:, sort_key],
-            user_input,
-            self._column_sort_status[sort_key],
-        )
--- stocks_monitor/main.py
+++ stocks_monitor/main.py
@@ -9,18 +9,19 @@ import pandas as pd
 import urwid
 
 from stocks_monitor.dataframe_widget import DataFrameWidget, SortKey
+from stocks_monitor.sort import sort_direction, SortStatus, df_sort_status
 
 
 def monitor(data_feed: Iterable[pd.DataFrame]) -> None:
 
     queue: Queue = Queue(1)
 
-    def update_loop() -> None:
+    def feed_loop() -> None:
 
         for data in data_feed:
             queue.put(data)
 
-    Thread(target=update_loop, daemon=True).start()
+    Thread(target=feed_loop, daemon=True).start()
     gui(queue)
 
 
@@ -29,26 +30,50 @@ def gui(queue: Queue) -> None:
     dataframe_widget = DataFrameWidget(
         pd.DataFrame(data=[], columns="Fetching data...".split())
     )
-    sort_key = SortKey()
 
     loop = urwid.MainLoop(
         widget=dataframe_widget,
         palette=[("bold", "light red,bold", "default")],
-        unhandled_input=dataframe_widget.handle_input(sort_key),
+        unhandled_input=dataframe_widget.handle_input(queue),
     )
 
-    Thread(target=draw_loop(queue, loop, sort_key), daemon=True).start()
+    Thread(target=update_loop(queue, loop), daemon=True).start()
     loop.run()
 
 
-def draw_loop(
-    queue: Queue, loop: urwid.MainLoop, sort_key: SortKey
-) -> Callable[[], None]:
+def update_loop(queue: Queue, loop: urwid.MainLoop) -> Callable[[], None]:
     def fn() -> None:
-
+        s_key = 0
+        arrival = queue.get()
+        if not isinstance(arrival, SortKey):
+            acting_on_input = False
+            data = arrival.sort_values(
+                by=arrival.columns[s_key],
+                ascending=sort_direction(
+                    series=arrival.iloc[:, s_key],
+                    acting_on_input=acting_on_input,
+                    sort_status=SortStatus(False, False),
+                ),
+            )
+            sort_signature = df_sort_status(data)
         while True:
-            loop.widget.data = queue.get()
-            loop.widget.sort_columns(sort_key.sort_key, user_input=False)
+            arrival = queue.get()
+            if isinstance(arrival, SortKey):
+                s_key = arrival.sort_key
+                acting_on_input = True
+            else:
+                data = arrival
+                acting_on_input = False
+            data = data.sort_values(
+                by=data.columns[s_key],
+                ascending=sort_direction(
+                    series=data.iloc[:, s_key],
+                    acting_on_input=acting_on_input,
+                    sort_status=sort_signature[s_key],
+                ),
+            )
+            sort_signature = df_sort_status(data)
+            loop.widget.update_columns(data)
             loop.draw_screen()
 
     return fn

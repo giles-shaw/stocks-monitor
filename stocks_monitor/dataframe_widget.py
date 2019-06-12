671048b055c4eb94ba5daa73b0a0b177c739c766
Giles Shaw <giles.shaw@gmail.com>
2019-06-12 19:59:06 +0100

    Add locking for thread safety, refactor
    Necessary because DataFrameWidget.sort_columns could be called from
    two threads at once

--- stocks_monitor/dataframe_widget.py
+++ stocks_monitor/dataframe_widget.py
@@ -2,6 +2,7 @@
 View a pd.DataFrame whose values are numbers or strings using an urwid widget
 with the ability to sort rows according to user input.
 """
+from threading import Lock
 from typing import Any, Callable
 
 import pandas as pd
@@ -16,30 +17,24 @@ class DataFrameWidget(urwid.Filler):
         original_widget = urwid.Columns([], dividechars=3)
         super().__init__(original_widget, "top")
         self.data = data
+        self.lock = Lock()
 
         self._column_sort_status = df_sort_status(self.data)
-        self.sort_columns(sort_key=0, acting_on_input=False)
+        self.sort_columns(sort_key=0, user_input=False)
 
-    def _sort_direction(self, sort_key: int, acting_on_input: bool):
+    def sort_columns(self, sort_key: int, user_input: bool) -> None:
 
-        return sort_direction(
-            self.data.iloc[:, sort_key],
-            acting_on_input,
-            self._column_sort_status[sort_key],
-        )
+        with self.lock:
+            sorted_df = self.data.sort_values(
+                by=self.data.columns[sort_key],
+                ascending=self._sort_direction(sort_key, user_input),
+            )
+            self._column_sort_status = df_sort_status(sorted_df)
 
-    def sort_columns(self, sort_key: int, acting_on_input: bool) -> None:
-
-        sorted_df = self.data.sort_values(
-            by=self.data.columns[sort_key],
-            ascending=self._sort_direction(sort_key, acting_on_input),
-        )
-        self._column_sort_status = df_sort_status(sorted_df)
-
-        self.original_widget.contents = [
-            (c, self.original_widget.options("pack"))
-            for c in format_df(sorted_df)
-        ]
+            self.original_widget.contents = [
+                (c, self.original_widget.options("pack"))
+                for c in format_df(sorted_df)
+            ]
 
     def handle_input(self, sort_key: SortKey) -> Callable[[Any], bool]:
         def fn(key: Any) -> bool:
@@ -54,7 +49,15 @@ class DataFrameWidget(urwid.Filler):
                 return False
 
             sort_key.sort_key = int(key) - 1
-            self.sort_columns(sort_key.sort_key, acting_on_input=True)
+            self.sort_columns(sort_key.sort_key, user_input=True)
             return True
 
         return fn
+
+    def _sort_direction(self, sort_key: int, user_input: bool):
+
+        return sort_direction(
+            self.data.iloc[:, sort_key],
+            user_input,
+            self._column_sort_status[sort_key],
+        )
